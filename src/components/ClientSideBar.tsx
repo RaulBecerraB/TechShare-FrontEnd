561d58c10db8d215cbe5f29b1790c3480c39226d
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-24 17:40:28 -0600

    feat (layout): add styles and semi-functional SideBar
        - Added styles for the layout.
        - Implemented a semi-functional SideBar.

---
+++ src/components/ClientSideBar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import SideBar from '@/components/SideBar';
+
+const items = ['Item 1', 'Item 2', 'Item 3'];
+
+const ClientSideBar: React.FC = () => {
+    const handleItemSelect = (item: string) => {
+        console.log('Selected item:', item);
+    };
+
+    return <SideBar items={items} onItemSelect={handleItemSelect} />;
+};
+
+export default ClientSideBar;
