28326002c99980610cafc50f01a2f0d8667170ff
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-02 17:09:34 -0600

    refactor: update SearchBar.tsx and search-bar.css to support icon addition
        - Refactored SearchBar.tsx and search-bar.css to ensure they function correctly with the new icon.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 import '@/styles/search-bar.css'
+import lupaIcon from '/src/assets/icons/lupa.svg'
 
 export default function SearchBar() {
   return (
-    <div>
-      <input type="text" className="search-input" placeholder="Search" />
-      <i className="search-icon">&#128269;</i>
+    <div className='search-container'>
+      <input type="text" className="search-bar" placeholder="Search"></input>
+      <Image src={lupaIcon} 
+      width={20}
+      height={20} 
+      alt="Search Icon"
+      className='search-bar-icon' />
     </div>
   )
 }
