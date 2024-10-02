8a5e80fce23aa049d0a1a65b21ceb0ebc199948d
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-02 17:30:45 -0600

    feat (components): add Dropdown component and filter icon
        - Added Dropdown.tsx component.
        - Added filter SVG to use as an icon.

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import '@/styles/dropdown.css'
+import '@/styles/containers.css'
+import lupaIcon from '/src/assets/icons/lupa.svg'
+
+export default function Dropdown() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
