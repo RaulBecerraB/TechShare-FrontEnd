46da6128d4de18d4e31b423041bf582adcaf8fa6
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-02 14:16:52 -0600

    feat (CommonPage.tsx): implement CRUD title design and button
        - Implemented design for CRUD title in CommonPage.tsx.
        - Added button design by modifying containers.css and buttons.css.

--- src/components/CommonPage/CommonPage.tsx
+++ src/components/CommonPage/CommonPage.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import '@/styles/containers.css'
+import '@/styles/buttons.css'
 import {inter} from '@/services/fonts'
 
 export default function CommonPage() {
   return (
     <div>
-        <div className={`white-container ${inter.className} antialiased font-semibold `} style={{ color: '#1E2A5E', fontSize: '1.5rem' }}>
+        <div className='white-container'>
+        <p className={` ${inter.className} antialiased font-semibold `} style={{ color: '#1E2A5E', fontSize: '1.5rem' }}>
             Sample
-            s
-            s
-            s
+        </p>
+
+        <button className='button'>
+            Button
+        </button>
         </div>
     </div>
   )
