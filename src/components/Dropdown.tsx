be6117112b0844f3aca7101bf4d5689e3d364de7
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-09 17:27:11 -0600

    style(Dropdown): increase padding to resemble search component
        - Increased the padding of the  component to match the styling of the search component.
        - The issue is not fully resolved yet, further adjustments are needed.

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -7,8 +7,8 @@ import filterIcon from '/src/assets/icons/filter.svg'
 export default function Dropdown() {
   return (
     <div className=''>
-      <div className='my-6 relative text-sm'>
-        <select className='dropdown ' defaultValue="">
+      <div className='my-6 relative '>
+        <select className='dropdown text-sm' defaultValue="">
           <option value="" disabled>Select an option</option>
           <option value="option1">Option 1</option>
           <option value="option2">Option 2</option>
