3412240fd6cf25b75acc73cb35eaea96c3d4122b
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-09 09:35:29 -0600

    feat(CrudBody): add example content and implement in roles page
        - Added example content to  to demonstrate its usage.
        - Implemented  in the roles page to display CRUD functionality.

--- src/app/admin/roles/page.tsx
+++ src/app/admin/roles/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import React from 'react'
 import CrudHeader from '@/components/AdminCrud/CrudHeader'
+import CrudBody from '@/components/AdminCrud/CrudBody'
 
 export default function roles() {
 
   return (
     <div>
       <CrudHeader title='Roles' buttonLabel='AÑADIR ROL' buttonFunction={() => console.log('hii')} />
+      <CrudBody />
     </div>
   )
 }
--- src/components/AdminCrud/CrudBody.tsx
+++ src/components/AdminCrud/CrudBody.tsx
@@ -3,7 +3,27 @@ import React from 'react'
 export default function CrudBody() {
     return (
         <div>
-
+            <div className='white-container'>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Admin</td>
+                            <td>Administrador</td>
+                            <td>
+                                <button className='button'>Editar</button>
+                                <button className='button'>Eliminar</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
