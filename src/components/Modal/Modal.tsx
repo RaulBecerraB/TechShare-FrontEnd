3aace839c73a802a445a91c88638f84956db261a
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-14 14:25:28 -0600

    fix(Modal): resolve hydration issues by restructuring HTML
        - Reorganized HTML structure of Modal component and removed table tags
        - Updated modal.css to ensure consistent appearance of the modal after changes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,33 +8,28 @@ import Button from '@/components/AdminCrud/PrimaryButton'
 export default function Modal() {
     return (
         <div className='white-container w-64'>
-            <table className='modal'>
-                <thead>
-                    <tr className='text-lg'>
-                        <th>Crear nuevo Rol</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td >
-                            <input className='mt-4 modal-input' type='text' />
-                        </td>
-                    </tr>
-                    <tr className='justify-end flex space-x-3 mt-4'>
-                        <Button
-                            buttonLabel='Cancelar'
-                            buttonFunction={() => console.log('Crear')}
-                            disabled={false}
-                            isSecondary={true}
-                        />
-                        <Button
-                            buttonLabel='Guardar'
-                            buttonFunction={() => console.log('Crear')}
-                            disabled={false}
-                        />
-                    </tr>
-                </tbody>
-            </table>
+            <div className='modal'>
+                <div className='border-b-[1px]'>
+                    <h2 className='text-lg'>Crear nuevo Rol</h2>
+                </div>
+
+                <div>
+                    <input className='mt-4 modal-input' type='text' placeholder="Nombre del rol" />
+                </div>
+                <div className='justify-end flex space-x-3 mt-2'>
+                    <Button
+                        buttonLabel='Cancelar'
+                        buttonFunction={() => console.log('Cancelar')}
+                        disabled={false}
+                        isSecondary={true}
+                    />
+                    <Button
+                        buttonLabel='Guardar'
+                        buttonFunction={() => console.log('Guardar')}
+                        disabled={false}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
