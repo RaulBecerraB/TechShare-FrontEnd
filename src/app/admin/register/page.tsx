b6add3a83ccf2b49664e659c0dd4e985af303878
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-17 15:29:53 -0600

    feat(register): import containers CSS and add base container to register page
        - Imported containers CSS for styling
        - Added a base container to the register page for layout consistency

---
+++ src/app/admin/register/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import '@/styles/containers.css'
+
+export default function register() {
+    return (
+        <div className="white-container">
+            <h1>register</h1>
+        </div>
+    );
+}
