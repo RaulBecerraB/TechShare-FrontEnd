c1f2a8f279ddc5adfe1fe59b2a25a377b5b79de0
Gabriel Hernández <rotsen.soher@gmail.com>
2024-10-18 18:18:56 -0600

    feat(sidenav): add new icons and categories to NavLinks. Updated 'NavLinks.tsx' and 'navlinks.json' files to reflect these changes.

--- src/components/SideNav/NavLinks.tsx
+++ src/components/SideNav/NavLinks.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link';
-import links from './navlinks.json'
-
+import links from './navlinks.json';
+import * as FaIcons from 'react-icons/fa';
 
 export default function NavLinks() {
   return (
     <div>
       {links.map((link) => {
-        const LinkIcon = link.icon;
+        const LinkIcon = FaIcons[link.icon];
         return (
           <div key={link.name}>
             <Link
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            <h2 className="font-medium text-xl" style={{ color: '#1E2A5E' }}>{link.name}</h2>
-          </Link>
+              href={link.href}
+              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            >
+              {LinkIcon && <LinkIcon />}
+              <h2 className="font-medium text-xl" style={{ color: '#1E2A5E' }}>{link.name}</h2>
+            </Link>
           </div>
         );
       })}
