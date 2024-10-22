ccf804b8a4c874b32d8418053a74be11b3ae71a4
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-22 08:55:01 -0600

    feat(login): add login request logic to login page (untested)
        - Implemented POST request logic for user login
        - Logic added but not yet tested for full functionality

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,25 +1,49 @@
 "use client"
 import React from 'react';
+import { useState } from 'react';
 import TextField from '@/components/Inputs/TextField';
 import PasswordField from '@/components/Inputs/PasswordField';
 import '@/styles/form.css';
 import '@/styles/buttons.css';
 
-const handleRegister = () => console.log('Registrando usuario...');
-const handleChange = () => console.log('Manejando cambios en los inputs...');
-const formData = {
-    email: '',
-    password: ''
-};
-
 const Page = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleLogin = (e: any) => {
+        e.preventDefault(); // Evitar que el formulario se envíe por defecto
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email: formData.email,
+            first_name: formData.password,
+        })
+    };
+
+    fetch('http://localhost:8080/login', requestOptions)
+        .then(response => response.text())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+
     return (
         <div>
             <div className="form-container">
                 <h1 className="text-primary font-bold">
                     CREA UNA CUENTA
                 </h1>
-                <form onSubmit={handleRegister}>
+                <form onSubmit={handleLogin}>
                     <TextField
                         placeholder="Correo electrónico"
                         name="email"
@@ -38,7 +62,7 @@ const Page = () => {
                         REGÍSTRATE
                     </button>
                 </form>
-                <p>¿Ya tienes una cuenta? <a href="#">Iniciar sesión</a></p>
+                <p>¿No tienes una cuenta? <a href="#">Regístrate</a></p>
             </div>
         </div>
     );
