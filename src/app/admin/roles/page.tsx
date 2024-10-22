9598a42faeeacc03ae29ca76cf69506847d27774
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-10-22 16:37:35 -0600

    feat(roles): add GET request logic for roles and update column filtering for 'Id' suffix
        - Implemented GET request to fetch roles on the roles page
        - Modified column filtering logic to exclude columns ending with 'Id'

--- src/app/admin/roles/page.tsx
+++ src/app/admin/roles/page.tsx
@@ -2,22 +2,39 @@
 import CrudHeader from '@/components/AdminCrud/CrudHeader'
 import CrudBody from '@/components/AdminCrud/CrudBody'
 import { useFetchData } from '@/services/useFetchData'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Modal from '@/components/Modal/Modal' // Importamos el Modal
 
 export default function roles() {
-  const dataUrl = '/dummyData/dummy.json'
-  const { data } = useFetchData<any>(dataUrl)
-
+  //const dataUrl = '/dummyData/dummy.json'
+  //const { data } = useFetchData<any>(dataUrl)
+  const [data, setData] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
-
   const [isModalVisible, setIsModalVisible] = useState(false)
 
   const handleSearchChange = (value: string) => setSearchTerm(value)
 
   const showModal = () => setIsModalVisible(true)
-
   const hideModal = () => setIsModalVisible(false)
+
+  const token = localStorage.getItem("sessionToken");
+
+  useEffect(() => {
+    fetchRoles(token)
+  }, [])
+
+  const fetchRoles = (token: any) => {
+    fetch("http://localhost:8080/admin/role/all", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error:", error));
+  }
 
   return (
     <div>
