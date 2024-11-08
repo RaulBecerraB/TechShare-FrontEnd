57a0d624a3fff9ef3da2351aaff5be9701459c67
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-11-08 10:57:38 -0600

    feat(roles): add fetch logic with useEffect for continuous data retrieval
    Implemented data fetching logic in the roles page, adding a useEffect to continually fetch updated content, ensuring real-time data visibility.

--- src/app/admin/movements/page.tsx
+++ src/app/admin/movements/page.tsx
@@ -1,21 +1,61 @@
 "use client"
 import CrudHeader from '@/components/AdminCrud/CrudHeader'
-import CrudBody from '@/components/AdminCrud/CrudBody'
+import CrudBody from '@/components/AdminCrud/CrudBodyViewOnly'
 import { useFetchData } from '@/services/useFetchData'
 import { useState } from 'react'
+import { useAuth } from '@/app/hooks/useAuth'
+import { getToken } from '@/services/storageService'
+import { useEffect } from 'react';
 
-export default function roles() {
+export default function movements() {
 
-  const dataUrl = '/dummyData/dummyUsers.json'
-  const { data } = useFetchData<any>(dataUrl);
+  type Movement = {
+    movementsId: number,
+    moveType: string,
+    quantity: number,
+    date: string,
+    comment: string,
+    usuarioId: number,
+    usuarioName: string,
+    materialsId: number,
+    materialsName: string
+  }
 
-  // Estado para el término de búsqueda
-  const [searchTerm, setSearchTerm] = useState('');
+  const [data, setData] = useState<Movement[]>([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useAuth()
+  const token = getToken()
+
+  const fetchMovements = (token: string) => {
+    //e.preventDefault();
+    console.log("Fetching movements");
+    fetch("http://localhost:8080/admin/movement/all", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data))
+
+  }
 
   // Función que actualiza el término de búsqueda
   const handleSearchChange = (value: string) => {
     setSearchTerm(value);
   };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (token) {
+        fetchMovements(token);
+      }
+    }, 5000); // Intervalo de 5 segundos
+
+    return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar el componente
+  }, [token]);
 
   return (
     <div>
